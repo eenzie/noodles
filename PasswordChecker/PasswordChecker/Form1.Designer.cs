ebd4071c013a9578331c1ace2f8a4ffbb28dd39f
eenzie <100802909+eenzie@users.noreply.github.com>
2023-07-12 21:47:24 +0200

    no message

--- PasswordChecker/PasswordChecker/Form1.Designer.cs
+++ PasswordChecker/PasswordChecker/Form1.Designer.cs
@@ -33,13 +33,18 @@
             label1 = new Label();
             btn_Test = new Button();
             tbx_Password = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
             SuspendLayout();
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(10, 70);
+            label2.Location = new Point(12, 193);
             label2.Name = "label2";
             label2.Size = new Size(49, 20);
             label2.TabIndex = 9;
@@ -48,26 +53,25 @@
             // tbx_Result
             // 
             tbx_Result.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            tbx_Result.Location = new Point(10, 92);
-            tbx_Result.Multiline = true;
+            tbx_Result.Location = new Point(204, 193);
             tbx_Result.Name = "tbx_Result";
-            tbx_Result.Size = new Size(766, 146);
+            tbx_Result.Size = new Size(273, 27);
             tbx_Result.TabIndex = 8;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(10, 24);
+            label1.Location = new Point(12, 145);
             label1.Name = "label1";
-            label1.Size = new Size(193, 20);
+            label1.Size = new Size(143, 20);
             label1.TabIndex = 7;
-            label1.Text = "Test your password strength";
+            label1.Text = "Enter your password";
             // 
             // btn_Test
             // 
             btn_Test.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            btn_Test.Location = new Point(613, 20);
+            btn_Test.Location = new Point(494, 138);
             btn_Test.Name = "btn_Test";
             btn_Test.Size = new Size(84, 35);
             btn_Test.TabIndex = 6;
@@ -78,16 +82,71 @@
             // tbx_Password
             // 
             tbx_Password.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            tbx_Password.Location = new Point(255, 24);
+            tbx_Password.Location = new Point(204, 142);
             tbx_Password.Name = "tbx_Password";
-            tbx_Password.Size = new Size(307, 27);
+            tbx_Password.Size = new Size(273, 27);
             tbx_Password.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 27);
+            label3.Name = "label3";
+            label3.Size = new Size(282, 20);
+            label3.TabIndex = 10;
+            label3.Text = "A password should contain the following:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(351, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(201, 20);
+            label4.TabIndex = 11;
+            label4.Text = "At least 16 characters in total";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(351, 48);
+            label5.Name = "label5";
+            label5.Size = new Size(126, 20);
+            label5.TabIndex = 12;
+            label5.Text = "At least 1 number";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(351, 68);
+            label6.Name = "label6";
+            label6.Size = new Size(186, 20);
+            label6.TabIndex = 13;
+            label6.Text = "At least 1 special character";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(351, 88);
+            label7.Name = "label7";
+            label7.Size = new Size(159, 20);
+            label7.TabIndex = 14;
+            label7.Text = "At least 1 capital letter";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(886, 376);
+            ClientSize = new Size(611, 248);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(tbx_Result);
             Controls.Add(label1);
@@ -95,7 +154,7 @@
             Controls.Add(tbx_Password);
             Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Password Checker";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -107,5 +166,10 @@
         private Label label1;
         private Button btn_Test;
         private TextBox tbx_Password;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
     }
 }
--- PasswordChecker/PasswordChecker/Form1.cs
+++ PasswordChecker/PasswordChecker/Form1.cs
@@ -11,18 +11,78 @@ namespace PasswordChecker
         {
             string password = tbx_Password.Text;
 
-            bool passwordTestResult = TestPassword(password);
+            bool isPasswordSecure = TestPassword(password);
 
-            if (passwordTestResult == true)
-                {
-                tbx_Result.Text = "Your password is secure";
+            if (!isPasswordSecure)
+            {
+                tbx_Result.BackColor = Color.Red;
+                tbx_Result.ForeColor = Color.Yellow;
+                tbx_Result.Text = "Your password is not secure";
             }
             else
             {
-                tbx_Result.Text = "Your password is not very secure";
+                tbx_Result.BackColor = Color.Green;
+                tbx_Result.ForeColor = Color.Yellow;
+                tbx_Result.Text = "Your password is secure!";
             }
         }
-        
+
+        static bool TestPasswordFormat(string password)
+        {
+            return !password.Contains(" ") && !string.IsNullOrEmpty(password);
+        }
+
+        static bool TestPasswordLength(string password)
+        {
+            return password.Length > 16;
+        }
+
+        static bool TestPasswordNumbers(string password)
+        {
+            return password.Any(char.IsDigit);
+        }
+
+        static bool TestPasswordSpecialCharacters(string password)
+        {
+            return password.Any(ch => !char.IsLetterOrDigit(ch));
+        }
+
+        static bool TestPasswordCapitalLetter(string password)
+        {
+            return password.Any(char.IsUpper);
+        }
+
+        public bool TestPassword(string password)
+        {
+            bool isPasswordValid = true;
+
+            if (!TestPasswordFormat(password))
+            {
+                isPasswordValid = false;
+            }
+
+            if (!TestPasswordLength(password))
+            {
+                isPasswordValid = false;
+            }
+
+            if (!TestPasswordNumbers(password))
+            {
+                isPasswordValid = false;
+            }
+
+            if (!TestPasswordSpecialCharacters(password))
+            {
+                isPasswordValid = false;
+            }
+
+            if (!TestPasswordCapitalLetter(password))
+            {
+                isPasswordValid = false;
+            }
+
+            return isPasswordValid;
+        }
 
     }
 }
